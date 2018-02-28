d6bed70d82af99852520b6af29e9f27b3d940aba
RedCraftGH <36905383+RedCraftGH@users.noreply.github.com>
2018-02-27 23:02:01 -0500

    Update CXBot.js

--- CXBot.js
+++ CXBot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
 
-
+let guild = message.guild
 
 bot.on("ready", () => {
 console.log('I am ready...');
@@ -20,8 +20,18 @@ bot.on('message', message => {
     embed.setColor("BLUE")
     embed.setDescription("PONG!\n" + Math.round(bot.ping) + "ms")
     message.channel.send({embed});
-    
   }
+  
+ const warns = member.guild.channels.find('name' , 'warns')
+ bot.channels.get(warns.id).send();
+
+if(!message.guild.member(message.author).hasPermission('MANAGE_SERVER') {
+  return;
+} else if (command === 'warn') {
+  let member = message.mentions.members.first();
+  message.warns('```User Warned: ${member}```');
+  message.channel.send('```User Warned: ${member}```');
+} else return;
 });
 
 
