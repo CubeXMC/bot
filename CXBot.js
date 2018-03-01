2148a7fd5b7961cb0bce2b19d88f5d31910dad7b
RedCraftGH <36905383+RedCraftGH@users.noreply.github.com>
2018-03-01 10:09:47 -0500

    Update CXBot.js

--- CXBot.js
+++ CXBot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
 
+var prefix = 'CX:'
 
 bot.on("ready", () => {
 console.log('I am ready...');
@@ -11,12 +12,12 @@ bot.user.setActivity("On CubeXNetwork",{type:"PLAYING"});
 bot.on('message', message => {
   if (message.author.bot) return;
 
-  const args = message.content.slice(config.prefix).trim().split(/ +/g);
+  const args = message.content.slice(prefix).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   let guild = message.guild
   const embed = new Discord.RichEmbed();
 
-  if (message.content.toLowerCase() === (config.prefix) + 'ping') {
+  if (message.content.toLowerCase() === (prefix) + 'ping') {
     embed.setColor("BLUE")
     embed.setDescription("PONG!\n" + Math.round(bot.ping) + "ms")
     message.channel.send({embed});
