7b3b9526aff707d9b74130cd954ccbbc5421b6b4
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-03-01 20:58:56 -0500

    Update CXBot.js

--- CXBot.js
+++ CXBot.js
@@ -15,7 +15,7 @@ bot.on('message', message => {
   let guild = message.guild
   var embed = new Discord.RichEmbed();
 
-  if (message.content.toLowerCase() == prefix + 'ping'){
+  if (message.content.toLowerCase() === prefix + 'ping'){
     embed.setColor("BLUE");
     embed.setDescription("PONG!\n" + Math.round(bot.ping) + "ms");
     message.channel.send({embed});
