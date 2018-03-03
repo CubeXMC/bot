def63fe59b2c49dbc59f9a9d54f971521f34568f
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-03-03 15:08:17 -0500

    Update CXBot.js

--- CXBot.js
+++ CXBot.js
@@ -15,9 +15,14 @@ bot.on('message', message => {
   let guild = message.guild
   var embed = new Discord.RichEmbed();
 
-  if (message.content.toLowerCase() === prefix + 'ping'){
+  if (message.content.toLowerCase() === prefix + 'ping') {
     embed.setColor("BLUE");
     embed.setDescription("PONG!\n" + Math.round(bot.ping) + "ms");
+    message.channel.send({embed});
+  }
+  if(message.content.toLowerCase() === prefix + 'serverinfo') {
+    embed.setColor('#7CFC00')
+    embed.setDescription('')
     message.channel.send({embed});
   }
   
