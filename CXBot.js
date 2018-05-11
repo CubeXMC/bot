dfde6fd4ecee87745c531bf5d63b5bb9004d9c57
BradGamez <33333609+BradGamez@users.noreply.github.com>
2018-05-11 17:17:44 -0400

    Update CXBot.js

--- CXBot.js
+++ CXBot.js
@@ -26,12 +26,13 @@ bot.on('message', message => {
     embed.setDescription('')
     message.channel.send({embed});
   }
+  if(message.author.id === `335893092756488205` || message.author.id === `320792865888534539` || message.author.id === `314146155564892160` || message.author.id === `332454451845660673` || message.author.id === `257330370490531840` || message.author.id === `334777598758617090` || message.author.id === `323908416248610817`) {
   if(message.content.toLowerCase().startsWith(prefix + 'general announcement')) {
     embed.setAuthor(`Announcement`,message.guild.iconURL)
     embed.setColor('#ed1c1c')
     embed.setDescription(`${args}`)
     bot.channels.get(`391052574842945548`).send({embed})
-  }
+  }}
   
 });
 
